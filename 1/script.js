4cdfb2972b17db4fc35a00a505edb722a6dcdab7
Mikhail Sharapov <147195026+ifeelfuckinggoooodmaaaan@users.noreply.github.com>
2023-10-07 10:40:25 +0300

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,12 @@
+let myText = 'The dog ate my homework!';
+alert(myText);
+
+myText = 'hello, world!';
+myText = 99;
+myText = 4 * 10;
+myText = true;
+myText = undefined;
+alert(myText);
+
+const siteURL='https://google.com'
+alert(siteURL)
---
+++ 3/script.js
@@ -0,0 +1,7 @@
+const name= "Николай";
+console.log("Привет, " + name);
+if (name.lenght>6) {
+console.log ("Ну и длиннющее же у вас имя!");
+} else {
+console.log("Имя у вас не из длинных");
+}
